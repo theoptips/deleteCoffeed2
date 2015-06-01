44db98b4cc9969a6b0d6807ab0270fa40cb6cb38
DILYS <optanovo@gmail.com>
2015-06-01 03:22:05 -0700

    fixing heroku db error

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -117,3 +117,13 @@ TEMPLATE_DIR = (
 STATICFILES_DIRS = (
     os.path.join(MAIN_DIR, 'static'),
 )
+
+# boto setting 
+# aws setting
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_S3_FORCE_HTTP_URL = True
+AWS_QUERYSTRING_AUTH = False
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWSSecretKey')
+AWS_ACCESS_KEY_ID = os.environ.get('AWSAccessKeyId')
+
+AWS_STORAGE_BUCKET_NAME = 'coffeedsun'
--- core/models.py
+++ core/models.py
@@ -1,7 +1,29 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+
+#ericn onemonth customize code to make django work with aws
+import os
+import uuid
+
+def upload_to_location(instance, filename):
+    blocks = filename.split('.')
+    ext = blocks[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    instance.title = blocks[0]
+    return os.path.join('uploads/', filename)
 
 # Create your models here.
 class Location(models.Model):
-	title = models.CharField(max_length=300)
-	description = models.TextField(null=True,blank=True)
-	created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=300)
+    description = models.TextField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    hours = models.TextField(null=True, blank=True)
+    image_file = models.ImageField(upload_to=upload_to_location, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+    	return self.title
+
+    def get_absolute_url(self):
+    	# return "location/" + str(self.id)+ "/detail/"
+    	return reverse(viewname="location_list", args=[self.id])
